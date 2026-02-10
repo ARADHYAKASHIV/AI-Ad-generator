97e127f4dcc0a992d6e4c6e4c3a1fcf9e9903222
Aradhya Kashiv <kashiv.aradhya@gmail.com>
2026-02-10 06:40:28 +0530

    1:19:06 maiin

--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -1,17 +1,50 @@
+import { useState } from "react"
 import Title from "../components/Title"
+import UploadZone from "../components/UploadZone"
 
 const Generator = () => {
+
+  const [name, setName] = useState('')
+  const [productName, setProductName] = useState('')
+  const [productDescription, setProductDescription] = useState('')
+  const [aspectRatio, setAspectRatio] = useState('9:16')
+  const [productImage, setProductImage] = useState<File | null>(null)
+  const [modelImage, setModelImage] = useState<File | null>(null)
+  const [userPrompot, setUserPrompot] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>, type: 'product' | 'model') => {
+    if (e.target.files && e.target.files[0]) {
+      if (type === 'product') {
+        setProductImage(e.target.files[0])
+      } else {
+        setModelImage(e.target.files[0])
+      }
+    }
+  }
+const handleGenerate = async (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  
+}
+
+
   return (
     <div className="min-h-screen text-white p-6 md:p-12 mt-28">
-        <form className="max-w-4xl mx-auto mb-40">
-            <Title heading="Create In-Context Image" description="Upload your model and product images to generate UGC"/>
-            <div>
-                left col
-            </div>
-            <div>
-                Right col
-            </div>
-        </form>
+      <form onSubmit={handleGenerate} className="max-w-4xl mx-auto mb-40">
+        <Title heading="Create In-Context Image" description="Upload your model and product images to generate UGC" />
+        <div className="flex gap-20 mx:sm-flex-col items-start justify-between">
+          {/* Left Col */}
+          <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
+            <UploadZone label="Product Image" file={productImage} onClear={() => setProductImage(null)} onChange={(e) => handleFileChange(e, 'product')} />
+            <UploadZone label="Model Image" file={modelImage} onClear={() => setModelImage(null)} onChange={(e) => handleFileChange(e, 'model')} />
+          </div>
+          {/* Right Col */}
+          <div className="">
+            <p>Right col</p>
+          </div>
+
+        </div>
+      </form>
     </div>
   )
 }
